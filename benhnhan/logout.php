7354bc02e39a04b0575d916c7d1b1f205dd2870a
VuMinhHieu <minghieuu@gmail.com>
2022-03-10 20:39:51 +0700

    Update

--- benhnhan/logout.php
+++ benhnhan/logout.php
@@ -1,7 +1,7 @@
 <?php 
 session_start();
-if(isset($_SESSION['LoginOK'])){
-    unset($_SESSION['LoginOK']);
+if(isset($_SESSION['LoginOKBenhNhan'])){
+    unset($_SESSION['LoginOKBenhNhan']);
     header('location:login_benhnhan.php');
     
 }
