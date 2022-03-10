b19c77cbafbe0f7af0f5ac597cfb70ec8e6bca4a
VuMinhHieu <minghieuu@gmail.com>
2022-03-10 22:35:45 +0700

    update backend

--- bs/ajaxbenhan.php
+++ bs/ajaxbenhan.php
@@ -1,8 +1,13 @@
 <?php
+session_start();
+if (!isset($_SESSION['LoginOK'])) {
+    header("location:loginbs.php");
+}
+$mabs=$_SESSION['LoginOK'];
     include("dbConfig.php");
     $a1=$_POST['data1'];
     
-        $query = "SELECT * FROM benhnhan where mabn LIKE '$a1%'  ";
+        $query = "SELECT * FROM benhnhan where mabn LIKE '$a1%' and bacsiphutrach='$mabs'";
         $result = mysqli_query($db, $query);
         if (mysqli_num_rows($result) > 0) {
             while ($row = mysqli_fetch_assoc($result)) {
