e6bbb36c71319e774341c2a178a7fb5f0aac8a62
VuMinhHieu <minghieuu@gmail.com>
2022-03-15 14:53:46 +0700

    update

--- BusinessLogic/benhnhan/logout.php
+++ BusinessLogic/benhnhan/logout.php
@@ -2,7 +2,7 @@
 session_start();
 if(isset($_SESSION['LoginOKBenhNhan'])){
     unset($_SESSION['LoginOKBenhNhan']);
-    header('location:../../UI/benhnhan/login_benhnhan.php');
+    header('location:../../UI/index.html');
     
 }
 ?>
